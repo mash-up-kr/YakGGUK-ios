d371357fb75c3eed994a62e74b9a7de0029974ad
Chipmunk <rhj4862@gmail.com>
2018-11-24 22:19:13 +0900

    폴더 구조 생성

---
+++ YakGGUK/views/alarmlist/AlarmListNavigationController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class AlarmListNavigationController : UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ YakGGUK/views/alarmlist/alarms/AlarmViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class AlarmViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
