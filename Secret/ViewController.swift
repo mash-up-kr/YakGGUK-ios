0f105f5fc14e9539a99858163cc33dd9d9dc2700
Chipmunk <rhj4862@gmail.com>
2018-11-24 18:43:30 +0900

    Initial Commit

---
+++ Secret/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Secret
+//
+//  Created by 김선재 on 24/11/2018.
+//  Copyright © 2018 team1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
