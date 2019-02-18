fa11def60d38264c4d0ae11082059b27eb039693
Chipmunk <rhj4862@gmail.com>
2019-02-19 07:35:06 +0900

    알람 뷰 파일 추가

---
+++ YakGGUK/views/alarm/AlarmViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AlarmViewController.swift
+//  YakGGUK
+//
+//  Created by 김선재 on 19/02/2019.
+//  Copyright © 2019 team1. All rights reserved.
+//
+
+import UIKit
+
+class AlarmViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ YakGGUK/views/alarm/view/AlarmMedicinePageView.swift
@@ -0,0 +1,21 @@
+//
+//  AlarmMedicinePageView.swift
+//  YakGGUK
+//
+//  Created by 김선재 on 19/02/2019.
+//  Copyright © 2019 team1. All rights reserved.
+//
+
+import UIKit
+
+class AlarmMedicinePageView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
